12515d9e6043d0ee4784624e0e7a89ed6c8ec142
Kpona <105780570+Kpona@users.noreply.github.com>
2022-06-20 23:27:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let height="5'11";
+let name='ISIAKPONA DANIEL';
+let country='Nigeria';
+
+console.log("I am known as "  + name);
+
+
+console.log("I am " +height +"feet tall");
+
+
+console.log("I am from "+ country)
